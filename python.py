1b6c99dd09efe5fa634d4e19ad40c5f7c0b5d3a9
Errow1241 <elianmedrano1024@gmail.com>
2025-10-01 16:08:54 -0500

    got bored

--- python.py
+++ python.py
@@ -2,20 +2,12 @@
 
 # My name is Elian and I used vim for these edits
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # i use arch btw
+
+upper = input("number to sum up to from 0: ")
+sum = 0
+
+for i in range(0,int(upper)+1):
+    sum += i
+
+print(sum)
